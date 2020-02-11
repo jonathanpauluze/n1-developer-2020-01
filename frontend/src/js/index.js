8f866a7b7a954d7b1cd9d685870dc5fbaca6aaf4
Jonathan Pauluze <jonathanpauluze@gmail.com>
2020-02-10 21:55:57 -0300

    add: lógica para trocar nome e valor

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,16 +1,24 @@
 const slides = [
   {
     page: 1,
-    title: 'Mortal Kombat'
+    title: 'Mortal Kombat',
+    price: 229.90,
+    description: 'Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.'
   },
   {
     page: 2,
-    title: 'Red Dead Redemption'
+    title: 'Red Dead Redemption',
+    price: 249.99,
+    description: 'Cacilds vidis litro abertis. Atirei o pau no gatis, atirei o pau no gatis per gatis num morreus. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget.'
   }
 ];
 
 const slideTitle = document.querySelector('.slide-title');
 const slideControl = document.querySelector('.slide-control');
+const gameTitle = document.querySelector('.game-title');
+const gamePrice = document.querySelector('.game-price');
+const gameCents = document.querySelector('.game-cents');
+const gameDescription = document.querySelector('.game-description');
 let currentSlide = 0;
 
 // Start Slick
@@ -23,8 +31,13 @@ $(document).ready(function(){
   $('.slider').on('beforeChange', () => {
     currentSlide++;
     slideInfo = slides[currentSlide % slides.length];
+    formattedPrice = slideInfo.price.toString().split('.')
+    formattedCents = formattedPrice[1].length < 2 ? `,${formattedPrice[1]}0` : `,${formattedPrice[1]}`;
 
-    slideTitle.innerText = slideInfo.title
-    slideControl.innerText = `${slideInfo.page} / ${slides.length}`
-  })
+    slideTitle.innerText = slideInfo.title;
+    slideControl.innerText = `${slideInfo.page} / ${slides.length}`;
+    gameTitle.innerText = slideInfo.title;
+    gamePrice.innerHTML = `R$ ${formattedPrice[0]}<span class="game-cents">${formattedCents}</span>`;
+    gameDescription.innerText = slideInfo.description;
+  });
 });
