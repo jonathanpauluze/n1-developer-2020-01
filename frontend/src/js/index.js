82fbe9d27c7bed3fbf280930a461cf66a2efe344
Jonathan <jonathanpauluze@gmail.com>
2020-02-14 17:30:47 -0300

    add: import do scroll.js

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,8 +1,10 @@
+import handleScroll from './scroll.js';
 import startSlides from './slides.js';
 import handleModal from './modal.js';
 import productsSlider from './products-slider.js';
 import contactModal from './contact-modal.js';
 
+handleScroll();
 startSlides();
 handleModal();
 productsSlider();
